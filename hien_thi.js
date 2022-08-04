0e1c9b97710454981e2255e0b02c9b26ad92ad44
unknown <you@example.com>
2022-08-04 11:26:10 +0700

    console.log

--- hien_thi.js
+++ hien_thi.js
@@ -1,8 +1,3 @@
-var intro=" hihihi";
-document.getElementById("intro").innerHTML= intro;
 function showMessage(){
-    alert('hi');
-}
-function showMessage(){
-    document.write("đây nè");
+    console.log('Xin chào!');
 }
